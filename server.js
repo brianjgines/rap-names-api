9acdea2602d56c2c8223e86348c972d5e6398d5e
Brian Gines <brianjgines@gmail.com>
2023-07-11 14:34:58 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers ={ 
+    '21 savage':{
+        'age': 29, 
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England' 
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+
+    },
+    'dylan':{
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    }
+}   
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+app.get('/api/:name', (request, response)=>{
+    const rapperName = request.params.name.toLowerCase()
+    if(rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else {
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running Port ${PORT}, better go catch it`)
+})
